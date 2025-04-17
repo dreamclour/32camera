865c58c592bfb66a83d1c8da2d66cb7a9f9f408f
dreamclour <52489925+dreamclour@users.noreply.github.com>
2025-04-17 23:25:01 +0300

    Delete OBJ directory

--- OBJ/segger_rtt.d
+++
@@ -1,6 +0,0 @@
-..\obj\segger_rtt.o: ..\RTT\SEGGER_RTT.c
-..\obj\segger_rtt.o: ..\RTT\SEGGER_RTT.h
-..\obj\segger_rtt.o: ..\RTT\SEGGER_RTT_Conf.h
-..\obj\segger_rtt.o: D:\keil5\ARM\ARMCC\Bin\..\include\stdlib.h
-..\obj\segger_rtt.o: D:\keil5\ARM\ARMCC\Bin\..\include\stdarg.h
-..\obj\segger_rtt.o: D:\keil5\ARM\ARMCC\Bin\..\include\string.h
